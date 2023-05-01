6fa1571974e976cc5370f249e7d2f644d3e249b5
Brian Perry <brian.perry@pantheon.io>
2023-05-01 15:07:36 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Path Prefix Test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
